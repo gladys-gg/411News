8becd819e58c8ba874d0b77e2e6f723ede6a95b6
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-29 16:06:50 +0300

    creating first api call

--- app/models/news.py
+++ app/models/news.py
@@ -7,6 +7,6 @@ class News:
         self.id = id
         self.name = name
         self.title = title
-        self.poster = poster
+        self.poster = "https://www.reuters.com/resizer/KmoBUq7O3ko1-HAK_opxKq_NvRA=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/CSTERWE76FL5JPXLJY5FWFDJOY.jpg"+ poster
         self.publishedAt = publishedAt
         self.content = content
