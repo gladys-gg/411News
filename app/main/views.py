a05aae990d1fc18b6982ff3989666e067a88e291
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-30 20:29:14 +0300

    added the route to the articles page

--- app/main/views.py
+++ app/main/views.py
@@ -16,14 +16,14 @@ def index():
     return render_template('index.html', sources = sources)
 
 # Views
-@main.route('/sources/<string:id>')
+@main.route('/articles/<sources_id>')
 def articles():
     '''
     View root page function that returns the index page and its data
     '''
 
-        # Getting popular news
-    articles = get_articles()
+        # Getting popular articles
+    articles = get_articles(sources_id)
 
     return render_template('articles.html', articles = articles)
     
